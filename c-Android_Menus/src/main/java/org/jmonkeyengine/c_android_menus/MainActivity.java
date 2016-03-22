6694a756c1f2ebc5430f4703193a2e29e259b4b3
iwgeric <iwgeric@gmail.com>
2016-03-22 09:34:00 -0400

    Remove unused code for immersive mode

--- c-Android_Menus/src/main/java/org/jmonkeyengine/c_android_menus/MainActivity.java
+++ c-Android_Menus/src/main/java/org/jmonkeyengine/c_android_menus/MainActivity.java
@@ -38,26 +38,6 @@ public class MainActivity extends AppCompatActivity implements JmeAndroidInterfa
 
 
     /*
-    For jME games, it is probably best to use the IMMERSIVE_STICKY mode.  Simply set the system
-    flags as shown below and the layout contents defined above will be shown "full screen".
-    Swiping down from the top of the screen will temporarily show the navigation and status
-    bars.
-     */
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus) {
-//            getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-    }
-
-    /*
     Inflate the xml file that defines the menu items
      */
     @Override
