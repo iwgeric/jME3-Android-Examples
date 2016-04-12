b1e3a22e0def1e4840e350b6ad58e9427c834e9a
iwgeric <iwgeric@gmail.com>
2016-04-12 18:54:12 -0400

    Updates to dpad to control character motion

--- g-Jaime_Demo/src/main/java/org/jmonkeyengine/g_jaime_demo/gamelogic/Input/TouchInputHandler.java
+++ g-Jaime_Demo/src/main/java/org/jmonkeyengine/g_jaime_demo/gamelogic/Input/TouchInputHandler.java
@@ -9,6 +9,8 @@ import com.jme3.input.TouchInput;
 import com.jme3.input.controls.TouchListener;
 import com.jme3.input.controls.TouchTrigger;
 import com.jme3.input.event.TouchEvent;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
@@ -38,9 +40,16 @@ public class TouchInputHandler extends BaseAppState implements TouchListener {
     private SelectablePicture dpad = null;
 
     private CustomChaseCamera camera = null;
+    private Quaternion horizCameraRotation = new Quaternion();
     private BetterCharacterControl phyControl = null;
     private Vector3f walkDirection = new Vector3f();
-    private float maxSpeed = 10f;
+    private Vector3f lookDirection = new Vector3f();
+    private Vector3f curWalkDirection = new Vector3f();
+    private Vector3f curLookDirection = new Vector3f();
+    private float maxSpeed = 2f;
+    private float maxRotation = 1f;
+    private float[] camAngles = new float[3];
+
 
     public TouchInputHandler(Spatial character) {
         camera = findChaseCamera(character);
@@ -93,21 +102,33 @@ public class TouchInputHandler extends BaseAppState implements TouchListener {
     @Override
     public void update(float tpf) {
         super.update(tpf);
-        if (dpad != null && dpad.getActivePointer() >= 0) {
-            phyControl.setWalkDirection(walkDirection);
-            logger.log(Level.INFO, "onTouch walkDirection:{0}, phy walkDirection: {1}, phy velocity: {2}",
-                    new Object[]{walkDirection, phyControl.getWalkDirection(), phyControl.getVelocity()});
-            if (phyControl.getPhysicsSpace() == null) {
-                logger.log(Level.INFO, "PhysicsSpace is null!!!");
-            }
+        if (dpad != null && dpad.getActivePointer() != null) {
+            curWalkDirection.set(walkDirection);
+            curWalkDirection.multLocal(maxSpeed);
 
+            // create a quat level with the ground
+            camera.getCameraRotation().toAngles(camAngles);
+            camAngles[0] = 0f;
+            horizCameraRotation.fromAngles(camAngles).normalizeLocal();
+
+            // rotate walk direction based on camera rotation level with the ground
+            horizCameraRotation.multLocal(curWalkDirection);
+
+            // set physics control to rotated walk direction
+            phyControl.setWalkDirection(curWalkDirection);
+            phyControl.setViewDirection(curWalkDirection);
+
+        } else {
+            // dpad is not active so set the physics to zero motion
+            // (leave view direction as is)
+            phyControl.setWalkDirection(Vector3f.ZERO);
         }
     }
 
     @Override
     public void onTouch(String name, TouchEvent event, float tpf) {
-        logger.log(Level.INFO, "onTouch type:{0}, pointer: {1}, X: {2}, Y: {3}, phyControl: {4}, camera: {5}",
-                new Object[]{event.getType(), event.getPointerId(), event.getX(), event.getType(), phyControl, camera});
+//        logger.log(Level.INFO, "onTouch type:{0}, pointer: {1}, X: {2}, Y: {3}, phyControl: {4}, camera: {5}",
+//                new Object[]{event.getType(), event.getPointerId(), event.getX(), event.getType(), phyControl, camera});
 
         if (phyControl == null || camera == null) {
             return;
@@ -115,54 +136,47 @@ public class TouchInputHandler extends BaseAppState implements TouchListener {
 
         switch (event.getType()) {
             case DOWN:
-                if (dpad.getActivePointer() >= 0) {
-                    // dpad is being controlled by some other pointer (ie. different finger)
-                    logger.log(Level.INFO, "onDown PointerID: {0}", dpad.getActivePointer());
+                logger.log(Level.INFO, "onTouch Down Event, event.pointer: {0}, dpad.pointer: {1}",
+                        new Object[]{event.getPointerId(), dpad.getActivePointer()});
+                if (dpad.getActivePointer() != null) {
+                    // dpad is already being controlled by some other pointer (ie. different finger)
                     return;
                 } else {
                     // Record the pointer used on this down event for future comparison.
                     // Important because of multi-touch support.
+                    // Check the position to see if the down event is inside the dpad.  If it is,
+                    // use the position to determine the speed and direction of motion.
                     if (dpad.checkSelect(event.getX(), event.getY())) {
                         dpad.setActivePointer(event.getPointerId());
 
                         Vector2f strength = dpad.getLocationRatioFromCenter(event.getX(), event.getY());
-                        walkDirection.set(-strength.x*maxSpeed, 0f, strength.y*maxSpeed);
-                        phyControl.setWalkDirection(walkDirection);
-
-                        logger.log(Level.INFO, "onTouch strength:{0}, walkDirection: {1}",
-                                new Object[]{strength, walkDirection});
-
+                        walkDirection.set(-strength.x, 0f, strength.y);
+                        lookDirection.set(walkDirection);
                     } else {
                         logger.log(Level.INFO, "Touch not in dpad");
                     }
                 }
                 break;
             case UP:
+                logger.log(Level.INFO, "onTouch Up Event, event.pointer: {0}, dpad.pointer: {1}",
+                        new Object[]{event.getPointerId(), dpad.getActivePointer()});
                 // If the pointerID matches the one recorded in the Down event, reset
                 // the walkDirection and stop the character motion.
-                if (dpad.getActivePointer() == event.getPointerId()) {
-                    dpad.setActivePointer(-1);
+                // Don't set the lookDirection so they character stays looking in the same direction
+                if (dpad.getActivePointer() != null && dpad.getActivePointer() == event.getPointerId()) {
+                    dpad.setActivePointer(null);
                     walkDirection.set(0f, 0f, 0f);
-                    phyControl.setWalkDirection(walkDirection);
-//                    logger.log(Level.INFO, phyControl.)
-                    phyControl.jump();
-                } else {
-                    camera.enableRotation(true);
                 }
                 break;
-//            case MOVE:
             case SCROLL:
-                logger.log(Level.INFO, "onTouch SCROLL for pointerID:{0}", event.getPointerId());
+//                logger.log(Level.INFO, "onTouch SCROLL for pointerID:{0}", event.getPointerId());
                 // If the current pointer is the same as the one recorded previously, then
                 // use the touch location to determine the walk direction and speed.
-                // Use SCROLL or MOVE events to detect finger motion while pressing down.
-                if (dpad.getActivePointer() == event.getPointerId()) {
+                // Use SCROLL events to detect finger motion while pressing down.
+                if (dpad.getActivePointer() != null && dpad.getActivePointer() == event.getPointerId()) {
                     Vector2f strength = dpad.getLocationRatioFromCenter(event.getX(), event.getY());
-                    walkDirection.set(-strength.x*maxSpeed, 0f, strength.y*maxSpeed);
-                    phyControl.setWalkDirection(walkDirection);
-
-                    logger.log(Level.INFO, "onTouch strength:{0}, walkDirection: {1}",
-                            new Object[]{strength, walkDirection});
+                    walkDirection.set(-strength.x, 0f, strength.y);
+                    lookDirection.set(walkDirection);
                 }
                 break;
             default:
@@ -172,14 +186,14 @@ public class TouchInputHandler extends BaseAppState implements TouchListener {
     }
 
     private void initDpad(AssetManager assetManager, int screenWidth, int screenHeight) {
-        // size the dpad image to be 25% of the smallest dimension
+        // Size the dpad image to be 50% of the smallest dimension
         // (width or height based on screen orientation)
         float picWidth;
         float picHeight;
         if (screenWidth >= screenHeight) {
-            picHeight = picWidth = screenHeight * 0.25f;
+            picHeight = picWidth = screenHeight * 0.5f;
         } else {
-            picHeight = picWidth = screenWidth * 0.25f;
+            picHeight = picWidth = screenWidth * 0.5f;
         }
         int zDepth = 5; // positive z value used to make sure the image is visible over other items
 
